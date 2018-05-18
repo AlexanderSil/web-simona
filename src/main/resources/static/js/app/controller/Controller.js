efa27ce4c484351d984ecdd3a177afb3a91ec6c3
Olexandr Senzhanov <oleksandr.senzhanov@fabware.com>
2018-05-18 04:35:40 +0300

    Many fixes for phase 1.

--- src/main/resources/static/js/app/controller/Controller.js
+++ src/main/resources/static/js/app/controller/Controller.js
@@ -497,9 +497,9 @@ simonaApp.controller('MainController', ['$scope', '$http', '$location', 'esriLoa
          **********************************************************************************************************/
         var socket;
         var createSocket = function() {
-            // var res = new WebSocket('ws://192.168.1.88:10102/');
+            var res = new WebSocket('ws://192.168.1.88:10102/');
             // var res = new WebSocket('ws://0.0.0.0:10102/');
-            var res = new WebSocket('ws://127.0.0.1:10103/');
+            // var res = new WebSocket('ws://127.0.0.1:10103/');
             res.onopen = function() {
                 console.log("Connect Websocket to Symona server.");
             };
